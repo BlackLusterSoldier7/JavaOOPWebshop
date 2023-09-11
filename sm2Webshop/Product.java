b17d980bf1df19a3d07c23828c09d18c7b01e6af
BlackLusterSoldier7 <135146216+BlackLusterSoldier7@users.noreply.github.com>
2023-09-11 23:52:17 +0200

    Add files via upload

---
+++ sm2Webshop/Product.java
@@ -0,0 +1,33 @@
+package sm2Webshop;
+
+public class Product {
+	
+	private String name; 
+	private double price; 
+	
+	
+	
+	public Product(String name, double price) {
+		
+		
+		this.name = name; 
+		this.price = price; 
+		
+	}
+	
+	
+	
+	public double getPrice() {
+		return price; 
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+
+}
